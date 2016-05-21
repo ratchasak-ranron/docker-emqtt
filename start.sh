72ce10c42f7e09fd9335bb60ca88ec8b736ccca0
Ratchasak Ranron <ratchasak.ranron@gmail.com>
2016-05-21 18:49:51 +0700

    Support emqtt cluster without network

--- start.sh
+++ start.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+
+if [ "$NODE_IP" = "localhost" ]; 
+then
+	HOSTNAME="$(hostname)"
+	NODE_IP="$(awk '/'$HOSTNAME'/ {print $1}' /etc/hosts)"
+fi
 sed -i 's/127.0.0.1/'"$NODE_IP"'/g' /emqttd/etc/vm.args
 /emqttd/bin/emqttd start
 sleep 5
 if [ -z ${MASTER+x} ]; then
-echo '$MASTER' is not set.
+	echo '$MASTER' is not set.
 else
-/emqttd/bin/emqttd_ctl cluster join emqttd@$MASTER;
+	MASTER_IP="$(awk '/'$MASTER'/ {print $1}' /etc/hosts)"
+
+	if [ "$MASTER_IP" != "" ]; then
+		MASTER=$MASTER_IP
+	fi
+	/emqttd/bin/emqttd_ctl cluster join emqttd@$MASTER;
 fi
 sleep 10 && tail -f --retry /emqttd/log/*
---
+++ up_dev.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker-compose -f ./docker-compose-dev.yml up -d emqtt1
+docker-compose -f ./docker-compose-dev.yml up -d emqtt2
